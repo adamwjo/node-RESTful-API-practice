7a2df74a299ea2b2af67e2b4516061266e024c82
adamwjo <adamwjo@gmail.com>
2019-03-06 02:25:28 -0500

    finalizing before deployment

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const restify = require('restify');
 const mongoose = require('mongoose');
 const config = require('./config');
+const path = require('path')
 
 const server = restify.createServer();
 
 // Middleware
 server.use(restify.plugins.bodyParser());
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  server.use(restify.plugins.serveStatic('client/build'));
+
+  server.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 
 
