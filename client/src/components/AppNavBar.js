490182331af8f73ac1546f08f64e5bd86432334c
adamwjo <adamwjo@gmail.com>
2019-03-06 16:11:16 -0500

    version 1.0.0

--- client/src/components/AppNavBar.js
+++ client/src/components/AppNavBar.js
@@ -33,7 +33,7 @@ export default class NavBar extends Component {
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className="ml-auto" navbar>
                                 <NavItem>
-                                    <NavLink href='/'>
+                                    <NavLink href='https://github.com/adamwjo/node-RESTful-API-practice'>
                                         github
                                     </NavLink>
                                 </NavItem>
